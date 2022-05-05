d9a65161c0a781b1afebddafe4aafdfddcec91c1
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-05-05 18:58:35 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import { useState } from 'react';
-import Login from './components/login/Login';
 import './firebase'
 import './App.scss';
+import AuthProvider from './components/utils/AuthProvider';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProtectedRoute from './components/utils/ProtectedRoute';
 
 function App() {
     const [user, setUser] = useState(null)
 
     return(
-        <div>
-            {user == null ? <Login setUser={setUser}/> : 'Welcome!'}
-        </div>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/login' element={<LoginPage />}/>
+                    <Route path='/register' element={<RegisterPage />}/>
+                    <Route 
+                        path='/' 
+                        element={
+                            <ProtectedRoute>
+                                <HomePage />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
 
