7bc6d60f2062e5477472f142d8c492e27933c322
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-05-05 14:14:06 +0200

    Add home, login and register pages

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div className='loginPage'>
+            LoginPage
+        </div>
+    )
+}
+
+export default LoginPage
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterPage = () => {
+    return (
+        <div className='registerPage'>
+            RegisterPage
+        </div>
+    )
+}
+
+export default RegisterPage
