b1e7085a7e16ef4eb12ee02b41e224be36d591d3
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-05-08 13:58:43 +0200

    Add images to login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,13 @@
-import Login from "../components/login/Login"
+import Login from '../components/login/Login'
+import image1 from '../images/vector1.png'
+import image2 from '../images/vector2.png'
+import '../styles/LoginPage.scss'
 
 const LoginPage = () => {
     return (
         <div className='loginPage'>
+            <img className='imageBottom' src={image1} alt='loginimage'></img>
+            <img className='imageTop' src={image2}alt='loginimage'></img>
             <Login />
         </div>
     )
