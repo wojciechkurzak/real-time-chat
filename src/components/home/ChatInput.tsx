89f9288ad5fdb5bf2261699608d965b40ca2ad4b
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-05-18 14:01:09 +0200

    Add functionality to add messages to database

--- src/components/home/ChatInput.tsx
+++ src/components/home/ChatInput.tsx
@@ -1,18 +1,32 @@
-import React, { BaseSyntheticEvent, useState } from 'react'
+import React, { BaseSyntheticEvent, useContext, useState } from 'react'
+import { collection, addDoc } from 'firebase/firestore'
+import { db } from '../../firebase'
 import sendIcon from '../../images/send.png'
 import '../../styles/ChatInput.scss'
+import { AuthContext } from '../utils/AuthProvider'
 
 const ChatInput = () => {
     const [value, setValue] = useState('')
+
+    const currentUser = useContext(AuthContext)
 
     const handleChange = (e: BaseSyntheticEvent): void => {
         setValue(e.target.value)
     }
 
+    const sendMessage = async () => {
+        if(value.length === 0) return
+        await addDoc(collection(db, 'messages'), {
+            uid: currentUser.uid,
+            message: value
+        })
+        setValue('')
+    }
+
     return (
         <div className='chatInput'>
             <input type='text' placeholder='Type here...' value={value} onChange={handleChange}/>
-            <div className='send'>
+            <div className='send' onClick={sendMessage}>
                 <img src={sendIcon} alt="send" />
             </div>
         </div>
