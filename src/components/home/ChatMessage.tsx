f9bd12940e7adc36921e0b4083f1a58cc4375a58
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-05-26 14:45:26 +0200

    Add styling for message with default icon

--- src/components/home/ChatMessage.tsx
+++ src/components/home/ChatMessage.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import defaultIcon from '../../images/defaulticon.png'
+import '../../styles/ChatMessage.scss'
 
 const ChatMessage = ({displayName, imageURL, message}: {displayName: String, imageURL: String, message: String}) => {
     return (
         <div className='message'>
-            <p>{displayName}</p>
-            <p>{message}</p>
+            <img src={defaultIcon} alt="default" />
+            <div className='text'>
+                <p>{displayName}</p>
+                <p>{message}</p>
+            </div>
         </div>
     )
 }
